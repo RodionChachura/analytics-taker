f2265a32350a23c839505018910c003400e85fc8
rodion <rodionaaafgh@gmail.com>
2019-01-04 19:35:31 +0300

    feature: get active users

--- HelloAnalytics.py
+++ HelloAnalytics.py
@@ -8,31 +8,10 @@ SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 KEY_FILE_LOCATION = 'client_secrets.json'
 VIEW_ID = '187148380'
 
-
-def initialize_analyticsreporting():
-  """Initializes an Analytics Reporting API V4 service object.
-
-  Returns:
-    An authorized Analytics Reporting API V4 service object.
-  """
-  credentials = ServiceAccountCredentials.from_json_keyfile_name(
-      KEY_FILE_LOCATION, SCOPES)
-
-  # Build the service object.
+def main():
+  credentials = ServiceAccountCredentials.from_json_keyfile_name(KEY_FILE_LOCATION, SCOPES)
   analytics = build('analyticsreporting', 'v4', credentials=credentials)
-
-  return analytics
-
-
-def get_report(analytics):
-  """Queries the Analytics Reporting API V4.
-
-  Args:
-    analytics: An authorized Analytics Reporting API V4 service object.
-  Returns:
-    The Analytics Reporting API V4 response.
-  """
-  return analytics.reports().batchGet(
+  response = analytics.reports().batchGet(
       body={
         'reportRequests': [
         {
@@ -43,36 +22,24 @@ def get_report(analytics):
         }]
       }
   ).execute()
+  print(response)
+  # for report in response.get('reports', []):
+  #   columnHeader = report.get('columnHeader', {})
+  #   dimensionHeaders = columnHeader.get('dimensions', [])
+  #   metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
 
+  #   for row in report.get('data', {}).get('rows', []):
+  #     dimensions = row.get('dimensions', [])
+  #     dateRangeValues = row.get('metrics', [])
 
-def print_response(response):
-  """Parses and prints the Analytics Reporting API V4 response.
+  #     for header, dimension in zip(dimensionHeaders, dimensions):
+  #       print(header + ': ' + dimension)
 
-  Args:
-    response: An Analytics Reporting API V4 response.
-  """
-  for report in response.get('reports', []):
-    columnHeader = report.get('columnHeader', {})
-    dimensionHeaders = columnHeader.get('dimensions', [])
-    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
-
-    for row in report.get('data', {}).get('rows', []):
-      dimensions = row.get('dimensions', [])
-      dateRangeValues = row.get('metrics', [])
-
-      for header, dimension in zip(dimensionHeaders, dimensions):
-        print(header + ': ' + dimension)
-
-      for i, values in enumerate(dateRangeValues):
-        print('Date range: ' + str(i))
-        for metricHeader, value in zip(metricHeaders, values.get('values')):
-          print(metricHeader.get('name') + ': ' + value)
-
-
-def main():
-  analytics = initialize_analyticsreporting()
-  response = get_report(analytics)
-  print_response(response)
+  #     for i, values in enumerate(dateRangeValues):
+  #       print('Date range: ' + str(i))
+  #       for metricHeader, value in zip(metricHeaders, values.get('values')):
+  #         print(metricHeader.get('name') + ': ' + value)
 
 if __name__ == '__main__':
   main()
+
